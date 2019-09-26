b4762237d789ef4d482818cc766e960f8c82bba4
mjfalahee <mjfalahee@gmail.com>
2019-09-26 15:49:04 -0700

    Chat messaging is styled slightly. I still need to figure out the overflow issue, as only the first 6 messages show up.

--- app/src/components/Chat.js
+++ app/src/components/Chat.js
@@ -1,16 +1,52 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import './InformationStyles.scss'
 
 const Chat = props => {
+    const [chatinput, setChatInput] = useState('')
+
+
     console.log(props)
+    let chats_to_use = props.chats
+
+    const sendChat = e => {
+        e.preventDefault()
+        console.log("Send Chat!")
+
+        const key = localStorage.getItem("key")
+        const auth = `Token ${key}`
+
+        let data_to_send = {
+            "chatmessage" : chatinput
+          }
+      
+          let config = {
+            headers: {
+              Authorization: auth 
+            }
+          } 
+      
+        axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/adv/say/`,
+        data_to_send,
+        config)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+        setChatInput('')
+    }
+
     return(
         <div className="chat-container">
             <h3 className="info-label">Chat</h3>
             <ul className="chat-message-list">
                 {props.chats.map(chat => {
                     if (chat.type === true) {
-                        console.log('NO #1')
                         return (
                             <li className="chat-message"> 
                                 <p className="chat-username">{chat.chatuser}:</p>
@@ -18,7 +54,6 @@ const Chat = props => {
                             </li>
                         )
                     } else {
-                        console.log('NO #2')
                         return (
                             <li className="event-message">
                                <p className="event-text">{chat.message}</p> 
@@ -27,6 +62,9 @@ const Chat = props => {
                     }
                 })}
             </ul>
+            <form className="chatbox-container"onSubmit={sendChat} >
+            <input className="chatbox" type="text" value={chatinput} onChange={e=> setChatInput(e.target.value)}/>
+            </form>
         </div>
     )
 }
