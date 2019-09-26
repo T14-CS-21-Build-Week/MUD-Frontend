be02d64f90727ea7b478305ee25aef9ca94ade4a
mjfalahee <mjfalahee@gmail.com>
2019-09-25 20:07:33 -0700

    added text to footer

--- app/src/components/BottomPanel.js
+++ app/src/components/BottomPanel.js
@@ -5,6 +5,7 @@ import './BottomPanel.scss'
 const BottomPanel = props => {
     return(
         <div className="bottom-panel-container">
+            <p>Created for a Build Week @LambdaSchool</p>
         </div>
     )
 }
