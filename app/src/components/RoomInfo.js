b59f45cb0da8be3a9ee3348f2549d3433dc253e7
mjfalahee <mjfalahee@gmail.com>
2019-09-25 19:54:53 -0700

    Text on TitleBar edited a bit, added a hover to links to make them more obvious

--- app/src/components/RoomInfo.js
+++ app/src/components/RoomInfo.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const RoomInfo = props => {
 
-
     return(
-        <>
+        <div className="room-info-container">
         <h1> RoomInfo </h1>
-        </>
+        <p></p>
+        </div>
     )
 }
 
